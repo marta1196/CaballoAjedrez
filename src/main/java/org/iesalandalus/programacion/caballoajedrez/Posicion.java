03c2d60ff31b927f1305c6816c24305718488f1b
marta <martuska1196@gmail.com>
2018-11-20 15:54:37 +0100

    He creado los metodos get y set

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -4,5 +4,39 @@ public class Posicion {
 	
 	private int fila;
 	private char columna;
+	
 
+	public Posicion() {
+	
+	}
+	
+	
+	public int getFila() {
+		return fila;
+	}
+
+	
+	public void setFila(int fila) {
+		if (fila<1 || fila>8) {
+			throw new IllegalArgumentException("la fila no existe");
+			}else {
+				this.fila=fila;
+			}
+	}
+
+	
+	public char getColumna() {
+		return columna;
+	}
+
+	
+	public void setColumna(char columna) {
+
+		if (columna<'a' || columna>'h') {
+		throw new IllegalArgumentException("la columna no existe");
+		}else {
+			this.columna=columna;
+		}
+	}
+	
 }
